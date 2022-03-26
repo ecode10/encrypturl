98bf38c0ae5099d934f01997bd7659f1bd35ad50
Mauricio Junior <mauricio.junior@gmail.com>
2022-03-26 15:34:34 -0500

    add project

---
+++ encryptdata2/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace encryptdata2.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public void OnPost()
+    {
+        //preparing data
+        var security = new CriptQuery.SecureQueryString();
+        security["email"] = "eu@mauriciojunior.net";
+        security["nome"] = "Mauricio Junior";
+        security["site"] = "https://mauriciojunior.net";
+        
+        //send to page 2
+        Response.Redirect("Privacy?id=" + security.ToString());
+    }
+}
---
+++ encryptdata2/Pages/Privacy.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace encryptdata2.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        var id = Request.Query["id"].ToString();
+
+        var security = new CriptQuery.SecureQueryString(id);
+
+        Response.WriteAsync($"Email: {security["email"].ToString()}");
+        Response.WriteAsync($"<br/> Nome: {security["nome"].ToString()}");
+        Response.WriteAsync($"<br/> Site: {security["site"].ToString()}");
+    }
+}
